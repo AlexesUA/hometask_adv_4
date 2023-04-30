f45c292b758756ec7eb18452f804cbdf81f8383f
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-30 13:29:00 +0300

    Created Main class

---
+++ src/ua/edu/cbs/lms/hometask_adv_4/task2/Main.java
@@ -0,0 +1,10 @@
+package ua.edu.cbs.lms.hometask_adv_4.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        JokeDecoder jokeDecoder = new JokeDecoder();
+
+        System.out.println("Text in source file before replace:");
+        jokeDecoder.printSourceFile();
+    }
+}
