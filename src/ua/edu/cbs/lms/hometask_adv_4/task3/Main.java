4c64a9b000f385b74ddc236c8433786706c89acb
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-30 14:47:32 +0300

    Created TimeElapsed class

---
+++ src/ua/edu/cbs/lms/hometask_adv_4/task3/Main.java
@@ -0,0 +1,5 @@
+package ua.edu.cbs.lms.hometask_adv_4.task3;
+
+public class Main {
+
+}
---
+++ src/ua/edu/cbs/lms/hometask_adv_4/task3/TimeElapsed.java
@@ -0,0 +1,19 @@
+package ua.edu.cbs.lms.hometask_adv_4.task3;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class TimeElapsed {
+    public static String timeElapsed(LocalDateTime fromTime, LocalDateTime toTime){
+        Duration duration = Duration.between(toTime, fromTime);
+        long years = duration.toDays() / 365;
+        long months = (duration.toDays() % 365) / 30;
+        long days = duration.toDays() % 30;
+        long hours = duration.toHours() % 24;
+        long minutes = duration.toMinutes() % 60;
+        long seconds = duration.getSeconds() % 60;
+        return "Вам виповнилося " + years + " років, " + months + " місяців, " + days + " днів, "
+                + hours + " годин, " + minutes + " хвилин та " + seconds + " секунд";
+
+    }
+}
